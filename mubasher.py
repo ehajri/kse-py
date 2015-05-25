f3599bf813b6479c17e9a833410153b17a839a78
Ebrahem Al-Hajeri <ehajri@Macintosh.local>
2015-05-25 23:12:56 +0300

    more about mubasher

--- mubasher.py
+++ mubasher.py
@@ -1,39 +1,43 @@
 #!/usr/local/bin/python3
-
-import anyjson
 import requests
 
-link = mo
-r = requests.get(link)
+# link = 'mo'
+# r = requests.get(link)
+# 
+# if r.status_code == requests.codes.ok:
+#     try:
+#         # print(r.text)
+#         # print(r.json())
+#         # print(o)
+#         v = 1
+#     except Exception as inst:
+#         print(type(inst))     # the exception instance
+#         print(inst.args)      # arguments stored in .args
+#         print(inst)
+#         pass
 
-if r.status_code == requests.codes.ok:
-    try:
-        # print(r.text)
-        # print(r.json())
-        o = anyjson.deserialize(r.json())
-        # print(o)
-    except Exception as inst:
-        print(type(inst))     # the exception instance
-        print(inst.args)      # arguments stored in .args
-        print(inst)
-        pass
+apis = ['listed-companies', 'amr', 'fairValues', 'capital-increase', 'earnings', 'stocks/prices']
+countries = ['kw', 'sa']
 
-def func(uri, lang='ar', c='kw', size=20, start=0, extra='', plain=False):
+def makehost(uri, lang='ar', c='kw', size=20, start=0, extra='', plain=False):
     if lang != 'ar' and lang != 'en':
         print('invalid language')
         return
-    
+
     host = '.mubasher.info/api/1/'
-    
+
     prefix = 'www' if lang == 'ar' else 'english'
     prefix = 'http://' + prefix
-    
+
     url = prefix + host + uri
     if not plain:
-        url += '?country=' + c + '&size=' + str(s) + '&' + extra
+        url += '?country=' + c + '&size=' + str(size) + '&start=' + str(start) + '&' + extra
     
+    return url
+
+def func(url):
     print('fetching %s..' % url)
-    
+
     try:
         r = requests.get(url)
         j = r.json()
@@ -43,31 +47,40 @@ def func(uri, lang='ar', c='kw', size=20, start=0, extra='', plain=False):
             print('rows: %d' % len(j['rows']))
         if 'numberOfPages' in j:
             print('pages: %d' % j['numberOfPages'])
+        
+        return j
     except:
         print('error')
         pass
-        
+
     return
 
-apis = ['listed-companies', 'amr', 'fairValues', 'capital-increase', 'earnings', ]
+def func2():
+    for api in apis:
+        print('Processing %s..' % api)
+        url = makehost(api, 'en', {})
+        p = func(url)
+        rows = p['rows']
+        pages = p['numberOfPages']
+        
+        i = len(rows)
+        j = pages * i
+        
+        for start in range(i, j, i):
+            d = {size: i, start: start}
+            h = makehost(api, size=i, start=start)
+            print(h)
+        break
+
+# 1 item = 1 page, 2 items = 1 page, 20 items = 1 page, 21 = 2 pages
+
+func2()
 
 # http://www.mubasher.info/api/1/events?countries=kw&from=2015-05-23&to=2015-05-30
-# http://www.mubasher.info/api/1/listed-companies?country=kw
-# http://www.mubasher.info/api/1/listed-companies?country=kw&size=20&start=20
-# http://www.mubasher.info/api/1/amr?country=kw
-# http://www.mubasher.info/api/1/amr?country=kw&size=20&start=20
 # http://www.mubasher.info/api/1/stocks/prices?country=kw
 # http://www.mubasher.info/api/1/stocks/prices/all?country=kw
-# http://www.mubasher.info/api/1/fairValues?country=kw
-# http://www.mubasher.info/api/1/fairValues?country=kw&size=20&start=20
 # http://www.mubasher.info/api/1/ipos?country=kw
 # http://www.mubasher.info/api/1/earnings?country=kw
 # http://www.mubasher.info/api/1/earnings?country=kw&size=20&start=20
 # http://www.mubasher.info/api/1/earnings?country=kw&year=2012
 # http://www.mubasher.info/api/1/earnings?country=kw&size=20&start=80&year=2012
-# http://www.mubasher.info/api/1/capital-increase?country=kw
-# http://www.mubasher.info/api/1/capital-increase?country=kw&size=20&start=20
-# 
-# 
-# 
-# earnings?country=kw&size=20&start=80&year=2012
